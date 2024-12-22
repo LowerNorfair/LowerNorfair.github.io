2948022ed034328e0c46a208cf618c37863d2657
LowerNorfair <88299726+LowerNorfair@users.noreply.github.com>
2024-12-23 10:54:44 +1100

    landing-page update

--- landing-page/README.md
+++ landing-page/README.md
@@ -1,15 +1,22 @@
 # Landing page - Dracula by Bram Stoker
-This project simulates an online book or article reader. 
+This project simulates an online book/article reader with a reactive table of contents.
 
 ## Usage
+
+
 Use the navigation to skip to different chapters of the book. 
 
 On smaller screens, the navigation can be open and closed by clicking the TOC button at the top right-hand corner of the screen.
 
 ## Demo
-[Index page](index.html)
+[Index page](github.com/lowernorfair/landing-page/index.html)
 
 ## Features
+- The script collects any h2 or h3 headings and creates the table of contents (TOC) based on their text
+- Clicking TOC links smooth-scrolls to the section
+- TOC highlights the active section
+- Inactive sections are faded
+- TOC shrinks on smaller screens
 
 ## Contact
 @Github [LowerNorfair](github.com/lowernorfair)
--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -5,8 +5,9 @@ const TOC_ACTIVE_CLASS = "nav-active-item";
 const TOC_EXPANDED_CLASS = "nav-expanded";
 const ACTIVE_SECTION = "content-section-active";
 
-let visibleSections = [];
-let prevActiveSection;
+// keeps track of sections inside the viewport
+let visibleSections = [];   
+let prevActiveSection; 
 
 /* 
 * addNav sets up the nav list by finding any h2 and h3 elements
@@ -17,6 +18,7 @@ function addNav(){
     const navList = document.querySelector(".section-nav");
     const sections = document.querySelectorAll(".content-section");
     let sublist = null;
+    let headings = [];
 
     if( sections.length <= 0 ) return;
     
@@ -41,19 +43,45 @@ function addNav(){
         else if(sectionHeading.matches("h2")) parent = fragment;
         
         // add the new nav element to its parent
-        addNavElement(sectionHeading, parent);
+        addNavElement(sectionHeading, parent, headings);
     });
 
     navList.appendChild(fragment);
 }
 
 /*
+* function for checking if there are duplicate headings
+* if there are 2 or more identical headings, new list elements are numbered
+*/
+function checkForDuplicate(title, headings){
+    let id = title;
+    
+    // if there's an existing identical heading
+    if(headings.includes(id)){
+        let version = 0;
+        const re = new RegExp(`${title}\d*`);
+
+        // check if there is more than one identical heading
+        headings.forEach((heading) =>{
+            if(re.test(heading)) version ++;
+        });
+
+        id = `${title}${version}`;
+    }
+
+    headings.push(id);
+
+    return id;
+}
+
+/*
 * addNavElement creates and adds a new list element to the nav
 */
-function addNavElement(item, parent){
+function addNavElement(item, parent, headings){
     // set section id anchor and data
     let title = item.textContent;
     let id = title.replace(/\W+/g, "-").toLowerCase().trim();
+    id = checkForDuplicate(id, headings);
     item.id = id;
     item.dataset.nav = `toc-${id}`;
     
@@ -68,14 +96,14 @@ function addNavElement(item, parent){
 * toggle the active nav item
 */
 function changeActiveNav(id){
-    const active = document.getElementsByClassName(`${TOC_ACTIVE_CLASS}`)[0];
+    const active = document.getElementsByClassName(TOC_ACTIVE_CLASS)[0];
     if(active) active.classList.toggle(TOC_ACTIVE_CLASS);
 
     document.getElementById(id).classList.toggle(TOC_ACTIVE_CLASS);
 }
 
 /*
-* section observer to keep track of what section is at the top of the screen
+* create section observer to keep track of what section is at the top of the screen
 */
 function createObserver(){
     let observer;
@@ -93,6 +121,8 @@ function createObserver(){
 
 /*
 * handleIntersect keeps track of visible sections and the active section
+* visible sections are tracked and sorted based on their bounding client
+* rect so that the top section is made active
 */
 function handleIntersect(entries, observer){
     // Add any sections comming into view, remove those going out
@@ -132,11 +162,14 @@ function handleIntersect(entries, observer){
 
 /*
 * adds smooth scroll to the nav links
+* scrollIntoView for any valid ids
+* scrollTo to scroll to the top of the page otherwise
 */
 function sectionSmoothScroll(){
     document.querySelector("nav").addEventListener("click", function(e){
         if(e.target.matches("a")){
             e.preventDefault();
+            // get the section ids from the anchor targets
             const id = e.target.href.split("#").pop();
             if(!id)
                 window.scrollTo({
@@ -156,7 +189,7 @@ function sectionSmoothScroll(){
 }
 
 /*
-* navExpand handles the nav when using a smaller screen
+* navExpand sets up a click listener to expand the TOC on smaller screens
 */
 function navExpand(){
     const navButton = document.querySelector(".expand-nav-btn");
